dd73aa6c543a59810a2d72c0e583540aa6ad1dd9
BenChisnell <ben.chisnell@my.jcu.edu.au>
2024-09-26 14:14:28 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,23 @@
-password_length = 4
-user_password = input("Enter your password: ")
-while len(user_password) < password_length:
-    print(f"Password doesn't meet minimum length, which is {password_length} characters.")
-    user_password = input("Enter your password: ")
+""" Ask the user for a password, with error-checking to repeat if the password doesn't meet a minimum length set by a variable."""
 
-for i in range(len(user_password)):
-    print("*", end="")
+
+def main():
+    password_length = 4
+    user_password = get_password(password_length)
+    print_password_as_astericks(user_password)
+
+
+def print_password_as_astericks(user_password):
+    for i in range(len(user_password)):
+        print("*", end="")
+
+
+def get_password(password_length):
+    user_password = input("Enter your password: ")
+    while len(user_password) < password_length:
+        print(f"Password doesn't meet minimum length, which is {password_length} characters.")
+        user_password = input("Enter your password: ")
+    return user_password
+
+
+main()
