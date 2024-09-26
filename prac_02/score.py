5802b6882b1d2b19ac81755d6cd28d6ea8c4acbf
BenChisnell <ben.chisnell@my.jcu.edu.au>
2024-09-26 14:49:29 +1000

    Add main function and generate a random score and print the result

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,16 +3,26 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    category = "Invalid"
-elif score < 50:
-    category = "Bad"
-elif score < 90:
-    category = "Pass"
-else:
-    category = "Excellent"
-print(f"Your score is {score}, which is {category}")
+import random
+def main():
+
+    score = float(input("Enter score: "))
+    category = determine_users_score(score)
+    print(f"Your score is {score}, which is {category}")
+    random_score = random.randint(0, 100)
+    print(f"Your random score is: {random_score}, which is {determine_users_score(random_score)}")
+
+def determine_users_score(score):
+    if score < 0 or score > 100:
+        category = "Invalid"
+    elif score < 50:
+        category = "Bad"
+    elif score < 90:
+        category = "Pass"
+    else:
+        category = "Excellent"
+    return category
 
 
+main()
 
