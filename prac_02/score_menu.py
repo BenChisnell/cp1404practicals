98a34e9cb16d7a978ea9d0595a7216999d4d5dc2
BenChisnell <ben.chisnell@my.jcu.edu.au>
2024-09-29 12:56:23 +1000

    Include menu option and functions

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,25 +1,59 @@
 """CP1404 - Programming II
-Get a valid score from a user (Between 0-100 inclusive), print that number and print that number of stars (*).
+Get a valid score from a user (Between 0-100 inclusive), print the score and score category, and print that score (number) of stars (*).
 """
 
-MENU = "(G)et a valid score (must be 0-100 inclusive) \n(P)rint result \n(S)how stars \n(Q)uit   "
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "G":
-        score = int(input("Score: "))
-        while score < 0 or score > 100:
-            print("Invalid score!")
-            score = int(input("Score: "))
-    elif choice == "P":
-        print(f"Score is: {score}")
-        print()
-    elif choice == "S":
-        for i in range(score):
-            print("*", end="")
-        print()
-    else:
-        print("Invalid choice")
+MENU = "(G)et a valid score (must be 0-100 inclusive) \n(P)rint result \n(S)how stars \n(Q)uit"
+
+
+def main():
+    score = get_valid_score()
     print(MENU)
     choice = input(">>> ").upper()
-print("Farewell.")
+    while choice != "Q":
+        if choice == "G":
+            score = get_valid_score()
+        elif choice == "P":
+            print_results(score)
+        elif choice == "S":
+            print_character(score)
+        else:
+            print("Invalid choice")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Farewell.")
+
+
+def get_valid_score():
+    score = int(input("Enter a valid score (0-100 inclusive): "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = int(input("Enter a valid score (0-100 inclusive): "))
+    print()
+    return score
+
+
+def determine_users_score(score):
+    if score < 0 or score > 100:
+        category = "Invalid"
+    elif score < 50:
+        category = "Bad"
+    elif score < 90:
+        category = "Pass"
+    else:
+        category = "Excellent"
+    return category
+
+
+def print_results(score):
+    category = determine_users_score(score)
+    print(f"Your score of {score} is {category}")
+    print()
+
+
+def print_character(score):
+    for i in range(score):
+        print("*", end="")
+    print()
+
+
+main()
