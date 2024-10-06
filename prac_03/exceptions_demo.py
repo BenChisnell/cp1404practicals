190958141d89ab8d00d112f5923aa822da460308
BenChisnell <ben.chisnell@my.jcu.edu.au>
2024-10-06 11:24:05 +1000

    Setup while loop for numerator & denominator variables to stop "0" value being entered by user

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -2,17 +2,30 @@
 CP1404/CP5632 - Practical
 Answer the following questions:
 1. When will a ValueError occur?
+if either the numerator or the denominator has data entered as a float.
+
 2. When will a ZeroDivisionError occur?
+When you try and divide the numerator with a denominator value of zero
+
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+Yes.
+
 """
 
 try:
     numerator = int(input("Enter the numerator: "))
+    while numerator == 0:
+        print("Invalid numerator")
+        numerator = int(input("Enter the numerator: "))
+
     denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        print("Invalid denominator")
+        denominator = int(input("Enter the denominator: "))
+
     fraction = numerator / denominator
     print(fraction)
 except ValueError:
     print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
+
 print("Finished.")
