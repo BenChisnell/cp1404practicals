53585e81c955ffe5958b727c9e2663015a7a00fb
BenChisnell <ben.chisnell@my.jcu.edu.au>
2024-10-12 16:35:44 +1000

    Completed Quick Picks exercise

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,22 @@
+"""
+CP1404 Practical
+Quick Picks
+"""
+import random
+
+MINIMUM = 1
+MAXIMUM = 45
+NUMBERS_PER_LINE = 6
+
+number_of_quick_picks = int(input("How many quick picks? "))
+"""Print random number of quick picks from user input"""
+for i in range(number_of_quick_picks):
+    numbers = []
+    for j in range(NUMBERS_PER_LINE):
+        number = random.randint(MINIMUM, MAXIMUM)
+        while number in numbers:
+            number = random.randint(MINIMUM, MAXIMUM)
+        numbers.append(number)
+    numbers.sort()
+
+    print(" ".join(f"{number:2}" for number in numbers))
