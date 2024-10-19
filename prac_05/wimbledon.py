8e6f2f0f6afcfc66c15c7c54871c5198892ce3d9
BenChisnell <ben.chisnell@my.jcu.edu.au>
2024-10-20 08:56:56 +1000

    Setup function to read input text file

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -2,8 +2,26 @@
 CP1404 Practical
 Wimbledon
 Estimate: 1 hour
-Actual:   51 minutes
+Actual:    minutes
 """
 
-with open("wimbledon.csv", encoding="utf-8-sig") as in_file:
+FILENAME = "wimbledon.csv"
 
+def main():
+    data = get_data(FILENAME)
+    print(f"{data}")
+
+
+def get_data(FILENAME):
+    data = []
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for line in in_file:
+            parts = line.strip().split(",")
+            data.append(parts)
+    return data
+
+
+
+
+main()
