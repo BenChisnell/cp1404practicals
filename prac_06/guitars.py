a26d1de3734ad75881fd9ee5237d6c7f04293ade
BenChisnell <ben.chisnell@my.jcu.edu.au>
2024-11-07 13:23:40 +1000

    compare my code versus a code review task

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -25,7 +25,7 @@ def main():
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
-    print("These are my guitars:")
+    print("\nThese are my guitars:")
     for i,guitar in enumerate(guitars, 1):
         vintage_string = ""
         if guitar.is_vintage():
