d68a0928b964f89802aa52e02f1787b351de5eca
BenChisnell <ben.chisnell@my.jcu.edu.au>
2024-11-08 20:29:15 +1000

    Add main function, add function to read from TXT file, add (len) to count number of projects in TXT file

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -5,34 +5,55 @@ Estimate: 2.5 hours
 Actual:
 """
 
+from operator import itemgetter
+
 MENU = "- (L)oad projects \n- (S)ave projects \n- (D)isplay projects  \n- (F)ilter projects by date \n- (A)dd new project \n- (U)pdate project\n- (Q)uit"
+FILENAME = "projects.txt"
 
-print("Welcome to Pythonic Project Management")
-print("Loaded 5 projects from projects.txt")
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "L":
-        pass
 
-    elif choice == "S":
-        pass
-
-    elif choice == "D":
-        pass
-
-    elif choice == "F":
-        pass
-
-    elif choice == "A":
-        pass
-
-    elif choice == "U":
-        pass
-
-    else:
-        print("Invalid choice")
+def main():
+    print("Welcome to Pythonic Project Management")
+    projects = get_data(FILENAME)
+    print(f"Loaded {len(projects)} projects from {FILENAME}")
     print(MENU)
     choice = input(">>> ").upper()
-print("Would you like to save to projects.txt?")
-print("Thank you for using custom-built project management software.")
+    while choice != "Q":
+        if choice == "L":
+            pass
+
+        elif choice == "S":
+            pass
+
+        elif choice == "D":
+            pass
+
+        elif choice == "F":
+            pass
+
+        elif choice == "A":
+            pass
+
+        elif choice == "U":
+            pass
+
+        else:
+            print("Invalid choice")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Would you like to save to projects.txt?")
+    print("Thank you for using custom-built project management software.")
+
+
+def get_data(filename):
+    """Extract data from TXT file and return it as a list"""
+    projects = []
+    with open(FILENAME, "r") as in_file:
+        for line in in_file:
+            parts = line.strip().split(",")
+            projects.append(parts)
+            # projects.sort(key=itemgetter(1))
+
+    return projects
+
+
+main()
