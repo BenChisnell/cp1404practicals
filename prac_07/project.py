6074c6f3c613f3534cbde002484db97f4ab12a4e
BenChisnell <ben.chisnell@my.jcu.edu.au>
2024-11-11 20:37:26 +1000

    Incomplete program

--- prac_07/project.py
+++ prac_07/project.py
@@ -4,3 +4,21 @@ Project Management Program
 Estimate: 2.5 hours
 Actual:
 """
+from datetime import datetime
+
+
+class Project:
+    def __init__(self, name="", start_date="", priority=0, estimate=0, completion=0):
+        self.name = name
+        self.start_date = datetime.strptime(start_date, "%d/%m/%Y")
+        self.priority = priority
+        self.estimate = estimate
+        self.completion = completion
+
+    def __str__(self):
+        """Print a string of Project information"""
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.estimate:.2f},completion: {self.completion}%"
+
+    def is_completed(self):
+        """Determine if a project is completed."""
+        return self.completion == 100
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,7 +4,7 @@ Project Management Program
 Estimate: 2.5 hours
 Actual:
 """
-
+from datetime import datetime
 from operator import itemgetter
 
 MENU = "- (L)oad projects \n- (S)ave projects \n- (D)isplay projects  \n- (F)ilter projects by date \n- (A)dd new project \n- (U)pdate project\n- (Q)uit"
@@ -19,19 +19,19 @@ def main():
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "L":
-            pass
+            load_projects(projects)
 
         elif choice == "S":
-            pass
+            save_project()
 
         elif choice == "D":
-            pass
+            display_projects(projects)
 
         elif choice == "F":
             pass
 
         elif choice == "A":
-            pass
+            add_new_project(projects)
 
         elif choice == "U":
             pass
@@ -44,16 +44,49 @@ def main():
     print("Thank you for using custom-built project management software.")
 
 
+def load_projects(projects):
+
+
 def get_data(filename):
     """Extract data from TXT file and return it as a list"""
     projects = []
     with open(FILENAME, "r") as in_file:
+        in_file.readline()
         for line in in_file:
-            parts = line.strip().split(",")
+            parts = line.strip().split('\t')
             projects.append(parts)
-            # projects.sort(key=itemgetter(1))
 
     return projects
+
+
+def add_new_project(projects):
+    name = input("Project name: ")
+    date = input("Date (DD/MM/YYYY): ")
+    priority = int(input("Priority: "))
+    estimate = int(input("Cost estimate: "))
+    completion = int(input("Completion: "))
+    add_to_project = ([name, date, priority, estimate, completion])
+    projects.append(add_to_project)
+
+
+def display_projects(projects):
+    incomplete_projects = [project for project in projects if not project.is_completed()]
+    completed_projects = [project for project in projects if project.is_completed()]
+
+    print("Incomplete projects: ")
+    for project in incomplete_projects:
+        print(project)
+
+    print("Completed projects: ")
+    for project in completed_projects:
+        print(project)
+
+
+def save_project():
+    name = input("Filename: ")
+    with open("name.txt", "w") as out_file:
+        print(name, file=out_file)
+        out_file.close()
 
 
 main()
